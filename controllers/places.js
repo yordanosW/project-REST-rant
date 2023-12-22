0def747ccde6a8341774737f00cb8498c71cb0a8
yordanos woldeyes <107896300+yordanosW@users.noreply.github.com>
2023-12-21 16:33:00 -0800

    Populating data fro star ratings

--- controllers/places.js
+++ controllers/places.js
@@ -77,6 +77,38 @@ router.post('/:id/comment', (req, res) => {
   })
 })
 
+router.delete('/:id', (req, res) => {
+  db.Place.findByIdAndDelete(req.params.id)
+  .then(place => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
+
+router.get('/:id/edit', (req, res) => {
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/edit', { place })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
+})
+
+router.put('/:id', (req, res) => {
+  db.Place.findByIdAndUpdate(req.params.id, req.body)
+  .then(() => {
+      res.redirect(`/places/${req.params.id}`)
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
+
 
 router.put('/:id', (req, res) => {
   res.send('PUT /places/:id stub')
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,24 @@ function show (data) {
       No comments Yet
     </h3>
   )
-  if (data.place.comments.length){
+  let rating =(
+    <h3 className="inacrive">
+      Not yet rated
+    </h3>
+  )
+  if(data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars }, 0)
+    let averageRating = Math.round(sumRatings / data.place.comments.length)
+    let stars  = ''
+    for (let i = 0; i < averageRating; i++){
+      stars += '⭐'
+    }
+    rating = (
+      <h3>
+        {stars} stars
+      </h3>
+    )
     comments= data.place.comments.map(
       c=>{
         return(
@@ -39,6 +56,7 @@ function show (data) {
       <h1>{ data.place.name }</h1>
             <section>
               <h2>Rating</h2>
+              {rating}
               <p>Not rated</p>
             </section>
             <section>
